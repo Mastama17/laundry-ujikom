6a505cb18660ab8380e7d5b87604cce2429e8eab
panggilsayabrp <berlianopratama@outlook.co.id>
2022-03-08 10:17:18 +0700

    update footer in login page

--- resources/views/layouts/auth/index.blade.php
+++ resources/views/layouts/auth/index.blade.php
@@ -69,12 +69,7 @@
         <footer class="py-4 bg-light mt-auto">
             <div class="container-fluid px-4">
                 <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright &copy; Your Website 2021</div>
-                    <div>
-                        <a href="#">Privacy Policy</a>
-                        &middot;
-                        <a href="#">Terms &amp; Conditions</a>
-                    </div>
+                    <div class="text-muted">Proudly hosted with <a href="https://forge.laravel.com">Laravel Forge</a> and <a href="https://digitalocean.com">Digital Ocean</a></div>
                 </div>
             </div>
         </footer>
